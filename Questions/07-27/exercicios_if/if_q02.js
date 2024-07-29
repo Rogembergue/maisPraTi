2214f1d102b780dce81ae8cf937238b643fd4ba3
unknown <rogemberguejr@gmail.com>
2024-07-29 19:57:45 -0300

    comecando a q3 if

---
+++ Questions/07-27/exercicios_if/if_q02.js
@@ -0,0 +1,8 @@
+const prompt = require("prompt-sync")();
+let ano = Number(prompt(`Insira o ano: `));
+
+if (ano%4==0 && ano%100!=0 || ano%400==0){
+    console.log(`Eh um ano bissexto!`);
+}else{
+    console.log(`Nao eh um ano bissexto!`);
+}
