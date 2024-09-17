e07e063b4855d8e5ceae631f0638dd8b0d570b16
unknown <rogemberguejr@gmail.com>
2024-09-16 21:31:37 -0300

    Finalizando! (Dessa vez é serio)

--- Questions/exercise_list_02/el02_q15.js
+++ Questions/exercise_list_02/el02_q15.js
@@ -1,6 +1,5 @@
 const prompt = require("prompt-sync")();
 
-// Criando um array de objetos transacoes
 const transacoes = [
     { tipo: 'entrada', valor: 500.00 },
     { tipo: 'saída', valor: 200.00 },
@@ -9,10 +8,8 @@ const transacoes = [
     { tipo: 'entrada', valor: 100.00 }
 ];
 
-// Variável para armazenar o saldo final
 let saldoFinal = 0;
 
-// Usando forEach para calcular o saldo final
 transacoes.forEach(transacao => {
     if (transacao.tipo === 'entrada') {
         saldoFinal += transacao.valor;
@@ -21,5 +18,4 @@ transacoes.forEach(transacao => {
     }
 });
 
-// Exibindo o saldo final
 console.log(`Saldo final: R$ ${saldoFinal.toFixed(2)}`);
